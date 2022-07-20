287dbbf39a7183ce0e850e47f150f41241228841
geek-Shayan <sadi06888@gmail.com>
2022-07-20 18:42:53 +0600

    reactive table with deletation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Movies from './components/movies';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <main className='container'>
+                <img src={logo} className="App-logo" alt="logo" />
+                <span><h1>Hello Movie World</h1></span>
+                <Movies/>
+            </main>  
+        
+        </div>
+    );
 }
 
 export default App;
